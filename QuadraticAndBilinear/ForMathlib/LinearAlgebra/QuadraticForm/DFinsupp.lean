d1dd9e22749f326f06d1d37d9eeaecd9608b4aaa
Eric Wieser <wieser.eric@gmail.com>
2025-07-24 11:51:43 +0000

    drop the sorry

--- QuadraticAndBilinear/ForMathlib/LinearAlgebra/QuadraticForm/DFinsupp.lean
+++ QuadraticAndBilinear/ForMathlib/LinearAlgebra/QuadraticForm/DFinsupp.lean
@@ -40,13 +40,38 @@ def QuadraticMap.dfinsupp [DecidableEq ι] :
         choose B hB using fun i => (Q i).exists_companion
         classical
         use DFinsupp.lsum ℕ fun i => LinearMap.flip <| DFinsupp.lsum ℕ fun j =>
-          if h : i = j then h ▸ B i else 0
-        intro x y
-        simp [DFinsupp.sumZeroHom_apply, DFinsupp.sumAddHom_apply, DFinsupp.sum,
-          hB, Finset.sum_add_distrib]
-
-
-        sorry}
+          Pi.single (M := fun j => Mᵢ j →ₗ[R] Mᵢ i →ₗ[R] N) i (B i).flip j
+        rintro ⟨x, sx, hx⟩ ⟨y, sy, hy⟩
+        dsimp [DFinsupp.sumAddHom, DFinsupp.sumZeroHom]
+        change ∑ xyi ∈ (sx + sy).toFinset, _ =
+          ∑ xi ∈ sx.toFinset, _ + ∑ y ∈ sy.toFinset, _ +
+            (∑ xi ∈ sx.toFinset, _ : _ →ₗ[_] _) _
+        rw [LinearMap.sum_apply]
+        simp [hB, Finset.sum_add_distrib]
+        congrm ?_ + ?_ + ?_
+        · refine Finset.sum_subset Finset.subset_union_left (fun i _ hi => ?_) |>.symm
+          rw [hx i |>.resolve_left (by simpa using hi), map_zero]
+        · refine Finset.sum_subset Finset.subset_union_right (fun i _ hi => ?_) |>.symm
+          rw [hy i |>.resolve_left (by simpa using hi), map_zero]
+        · change _ = ∑ xi ∈ sx.toFinset, (∑ yi ∈ sy.toFinset, _ : _ →ₗ[_] _) _
+          dsimp
+          simp_rw [LinearMap.sum_apply]
+          conv_rhs =>
+            enter [2, xi, 2, yi]
+            rw [
+              Pi.apply_single
+                (f' := fun i (f : Mᵢ i →ₗ[R] Mᵢ xi →ₗ[R] N) => DFunLike.coe f (y i))
+                (fun i => rfl) xi (B xi).flip yi,
+              Pi.apply_single (M := fun i ↦ Mᵢ xi →ₗ[R] N)
+                (f' := fun i (f : Mᵢ xi →ₗ[R] N) => DFunLike.coe f (x xi))
+                (fun i => rfl) xi ((B xi).flip (y xi)) yi,
+              LinearMap.flip_apply]
+          simp only [Finset.sum_pi_single']
+          rw [← Finset.sum_filter, Finset.filter_mem_eq_inter]
+          rw [← Finset.sum_subset Finset.inter_subset_union]
+          intros i _ hi
+          simp only [Finset.mem_inter, Multiset.mem_toFinset, not_and_or] at hi
+          obtain h | h := hi.imp (hx i |>.resolve_left) (hy i |>.resolve_left) <;> simp [h] }
   map_add' Q₁ Q₂ := by
     ext x
     classical
